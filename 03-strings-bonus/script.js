0c7393557b227abc60fc2696c337a7e0db950fde
martinolanza03 <lanzamartino3@gmail.com>
2025-02-21 15:58:21 +0100

    feat: ex1 bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,8 +6,8 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 0;
-console.log(cityIndex); // Output: 4
+const cityIndex = cityName;
+console.log(cityIndex.search("y")); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
